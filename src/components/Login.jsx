914cd1700b955c090a2411ce7bd9bc0e8ce0ddad
huan.nguyen <huan.nguyen@onemount.com>
2023-05-06 18:03:43 +0700

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,73 @@
-import React from "react";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import { FcGoogle } from "react-icons/fc";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { useGoogleLogin } from "@react-oauth/google";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import { FcGoogle } from "react-icons/fc";
+import { client } from "../client";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  const responesGoogle = (responese) => {};
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+  const navigate = useNavigate();
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      console.log(codeResponse);
+      setUser(codeResponse);
+    },
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          const { name, id, picture } = res.data;
+          const doc = {
+            _id: id,
+            _type: "user",
+            userName: name,
+            image: picture,
+          };
+
+          client.createIfNotExists(doc).then(() => {
+            navigate("/", { replace: true });
+          });
+          console.log(name, id, picture);
+
+          setProfile(res.data);
+        })
+        .catch((err) => console.log("Huan" + err));
+    }
+  }, [user]);
+  // const responseGoogle = (response) => {
+  //   console.log(response);
+
+  //   localStorage.setItem("user", JSON.stringify(response.profileObj));
+  //   console.log(response.profileObj);
+  // const { name, googleId, imageUrl } = response.profileObj;
+
+  // const doc = {
+  //   _id: googleId,
+  //   _type: "user",
+  //   userName: name,
+  //   image: imageUrl,
+  // };
+
+  // client.createIfNotExists(doc).then(() => {
+  //   navigate("/", { replace: true });
+  // });
+  // };
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -24,22 +87,14 @@ const Login = () => {
           </div>
 
           <div className="shadow-2xl">
-            <GoogleOAuthProvider clientId="">
-              <GoogleLogin
-                render={(renderProps) => (
-                  <button
-                    type="button"
-                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    <FcGoogle className="mr-4" /> Sign in with Google
-                  </button>
-                )}
-                onSuccess={responesGoogle}
-                onError={responesGoogle}
-              />
-            </GoogleOAuthProvider>
+            <button
+              type="button"
+              className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onClick={() => login()}
+            >
+              <FcGoogle className="mr-4" /> Sign in with google
+            </button>
+            {/* <GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} /> */}
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <GoogleOAuthProvider
+        clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+    >
+        <React.StrictMode>
+            <Router>
+                <App />
+            </Router>
+        </React.StrictMode>
+    </GoogleOAuthProvider>
 );
